c29acf22608178716a35605eaed6a4f1950f5f38
Sangram <sangrammukherjee90@gmail.com>
2023-06-11 14:09:36 +0530

    update

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,24 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/home/sangram/work/ayo/src/app/page.tsx": [
+      "/home/sangram/work/ayo/src/app/Components/Navbar/navbar.module.css",
+      "/home/sangram/work/ayo/src/app/Components/PageBottom/pagebottom.css",
+      "/home/sangram/work/ayo/src/app/Components/Footer/footer.css",
+      "/home/sangram/work/ayo/src/app/Components/Contact/Contact.css"
+    ],
+    "/home/sangram/work/ayo/src/app/layout.tsx": [
+      "/home/sangram/work/ayo/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/home/sangram/work/ayo/src/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/home/sangram/work/ayo/src/app/page": [
+      "/home/sangram/work/ayo/src/app/Components/Navbar/navbar.module.css",
+      "/home/sangram/work/ayo/src/app/Components/PageBottom/pagebottom.css",
+      "/home/sangram/work/ayo/src/app/Components/Footer/footer.css",
+      "/home/sangram/work/ayo/src/app/globals.css",
+      "/home/sangram/work/ayo/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/home/sangram/work/ayo/src/app/Components/Contact/Contact.css"
+    ]
+  }
+}
--- src/app/Components/Contact/Contact.tsx
+++ src/app/Components/Contact/Contact.tsx
@@ -1,15 +1,19 @@
+"use client"
 import React from 'react';
 import './Contact.css';
 import Image from 'next/image';
 import contact_us from '../../../../public/assets/Images/contact_us.png'
 import { useForm } from 'react-hook-form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 function Contact() {
 
-    // const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  // const onSubmit = data => console.log(data);
-  
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+
   return (
     <div className='mainBox'>
         <div className="box">
@@ -36,8 +40,14 @@ function Contact() {
               <div className="sub-text">
                 <p>24/7 We will answeryour questions and car parking problems</p>
               </div>
-              <form action="">
-
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <input type='text' placeholder='First Name' {...register('firstName')} />
+                <input type='text' placeholder='Last Name' {...register('lastName')} />
+                <input type="email" placeholder='Email' {...register('email')} /> 
+                <input type="tel" placeholder='Phone' {...register('mobile')} />
+                <input type='text' placeholder='Describe your query here' {...register('query')} />
+                {/* <input type='submit' className='btn' /> */}
+                <button type='submit' className='btn'>Send</button>
               </form>
               <h1>FORM</h1>
             </div>
